11e66f47bcb555816f786f8af25e99a412104d9b
Kisore Subburaman <49161209+Kisore-99@users.noreply.github.com>
2020-01-07 10:10:59 +0530

    Update schema.js

--- schema.js
+++ schema.js
@@ -48,65 +48,10 @@ const News= new GraphQLObjectType({
 });
 
 
-// // Launch type
-// const LaunchType = new GraphQLObjectType({
-//     name:'Launch',
-//     fields:()=>({
-//         flight_number: {type: GraphQLInt},
-//         mission_name: {type: GraphQLString},
-//         launch_year: {type: GraphQLString},
-//         launch_date_local: {type: GraphQLString},
-//         launch_success: {type: GraphQLBoolean},
-//         rocket: {type: RocketType}
-//     })
-// });
-
-// //Rocket Type
-// const RocketType = new GraphQLObjectType({
-//     name:'Rocket',
-//     fields:()=>({
-//         rocket_id: {type: GraphQLString},
-//         rocket_name: {type: GraphQLString},
-//         rocket_type: {type: GraphQLString},
-//     })
-// });
-
 //Root Query
 const RootQuery= new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
-        // launches: {
-        //     type: new GraphQLList(LaunchType),
-        //     resolve(parent, args){
-        //         return axios.get('https://api.spacexdata.com/v3/launches')
-        //         .then(res=> res.data);
-        //     }
-        // },
-        // neos:{
-        //     type: Neo_Feeds,
-        //     args: {
-        //         api_key: {type: GraphQLString},
-        //         START: {type: GraphQLString},
-        //         END: {type: GraphQLString},
-        //     },
-        //     resolve(parent, args){
-        //         return axios.get(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${args.START}&end_date=${args.END}&api_key=${args.api_key}`)
-        //         .then(res=>res.data);
-        //     }
-        // },
-        // newsap:{
-        //     type: News_Feed,
-        //     args: {
-        //         topic: {type: GraphQLString},
-        //         API_Token: {type:GraphQLString},
-        //     },
-        //     resolve(parent,args){
-        //         return axios.get(`https://gnews.io/api/v3/topics/${args.topic}?token=${args.API_Token}`)    
-        //         .then(res=>res.data);
-        //     }
-            
-        
-        // },
             //google news api
         newsap:{
             type: News_Feed,
@@ -131,33 +76,7 @@ const RootQuery= new GraphQLObjectType({
 //                     }
 //         },
  
-        // launch: {
-        //     type: LaunchType,
-        //     args: {
-        //         flight_number: {type: GraphQLInt}
-        //     },
-        //     resolve(parent, args){
-        //         return axios.get(`https://api.spacexdata.com/v3/launches/${args.flight_number}`)
-        //         .then(res=> res.data);
-        //     }
-        // },
-        // rockets: {
-        //     type: new GraphQLList(RocketType),
-        //     resolve(parent, args){
-        //         return axios.get('https://api.spacexdata.com/v3/rockets')
-        //         .then(res=> res.data);
-        //     }
-        // },
-        // rocket: {
-        //     type: RocketType,
-        //     args: {
-        //         id: {type: GraphQLInt}
-        //     },
-        //     resolve(parent, args){
-        //         return axios.get(`https://api.spacexdata.com/v3/rockets/${args.id}`)
-        //         .then(res=> res.data);
-        //     }
-        // }
+       
    } 
   });
 
